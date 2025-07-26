4fbd751133d6a8b000475c803a90ef5bfd915085
Jeff Wang <229298746@qq.com>
2025-07-26 17:52:03 +0800

    fix: react uses separate import

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,7 @@
 export * from './ignores';
 export * from './javascript';
 export * from './prettier';
+export * from './react';
 export * from './stylistic';
 export * from './typescript';
 export * from './vue';
